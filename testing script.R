1a4c4a23b3a75574a5f64873d81852582b6c8ef7
Zhihao-Pan <johnpanzhihao@gmail.com>
2023-04-21 21:20:20 +0800

    testing again

--- testing script.R
+++ testing script.R
@@ -1,8 +1,3 @@
 print("This file was created within RStudio")
 
 print("And now it lives on GitHub")
-
-git remote add origin git@github.com:Zhihao-Pan/Rstudio-testing.git
-  
-install.packages("gitcreds")
-gitcreds::gitcreds_set()
